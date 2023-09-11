223d9601a3a4b5bd6e918df2fe7a68a65f6ac33f
henrique-hnx <130366433+henrique-hnx@users.noreply.github.com>
2023-09-11 08:50:51 -0300

    Add files via upload

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,1 +1,13 @@
 console.log("hnx")
+//-------------------------------------- Daqui pra baixo começa o código-------------
+
+//alteração do titulo
+
+//console.log(document);
+//cria a variavel titulo e armazena  o  conteudo da classe titulo nela
+let titulo = document.querySelector (".titulo");
+//console.log(titulo);
+//atribuição de um novo valor na variavel titulo 
+titulo.textContent = "Trembolona Nutri"
+
+//alteração da tabela
