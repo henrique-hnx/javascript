a622528016e39c16a8983dca39912bd2b8f43a7f
enrike <130366433+henrique-hnx@users.noreply.github.com>
2023-09-12 12:27:27 -0300

    Add files via upload

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,13 +1,26 @@
-console.log("hnx")
-//-------------------------------------- Daqui pra baixo começa o código-------------
+console.log("O arquivo separado está funcionando")
 
-//alteração do titulo
+//--------- Daqui para baixo 
+// alteração do titulo
+//console.log(document)
+//cria a variavel titulo e armazena o conteudo de classe titulo nela
 
-//console.log(document);
-//cria a variavel titulo e armazena  o  conteudo da classe titulo nela
-let titulo = document.querySelector (".titulo");
-//console.log(titulo);
-//atribuição de um novo valor na variavel titulo 
-titulo.textContent = "Trembolona Nutri"
+let titulo = document.querySelector('.titulo');
+
+//console.log(titulo)
+//atribuição de um novo valor na variável titulo
+
+titulo.textContent = "Zé do bill nutrição"
 
 //alteração da tabela
+
+let paciente = document.querySelector ("#primeiro-paciente")
+ 
+let tdPeso = paciente.querySelector(".info-peso");
+let tdAltura = paciente.querySelector(".info-altura");
+
+let peso = tdPeso.textContent;
+let altura = tdAltura.textContent;
+
+let imc = peso / (altura * altura);
+console.log (imc)
